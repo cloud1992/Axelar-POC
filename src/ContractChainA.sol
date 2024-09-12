dac2f0f2f43c0503b39cc9f6d94aa84da072d6b3
cloud1992 <arturocollado@ratherlabs.com>
2024-09-12 17:17:01 -0300

    :boom: added contracts to test sendin ga message from A to B to C

---
+++ src/ContractChainA.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol";
+
+contract ExecutableSampleChainA is AxelarExecutable {
+    string public message;
+    IAxelarGasService public immutable gasService;
+
+    constructor(
+        address gateway_,
+        address gasReceiver_
+    ) AxelarExecutable(gateway_) {
+        gasService = IAxelarGasService(gasReceiver_);
+    }
+
+    // Call this function to send a message to chain destinationChain and destinationAddress.
+    // Also send send the information to chain destinationChainC and destinationAddressC in order to send a message to chain C from chain B
+    function sendMessage(
+        string calldata destinationChain,
+        string calldata destinationAddress,
+        string calldata messageToSend,
+        uint gasLimit,
+        string calldata destinationChainC,
+        string calldata destinationAddressC,
+        string calldata messageTochainC
+    ) external payable {
+        require(msg.value > 0, "Gas payment is required");
+
+        bytes memory payload = abi.encode(
+            messageToSend,
+            messageTochainC,
+            destinationChainC,
+            destinationAddressC
+        );
+        gasService.payGas{value: msg.value}(
+            address(this),
+            destinationChain,
+            destinationAddress,
+            payload,
+            gasLimit,
+            true,
+            msg.sender,
+            new bytes(0)
+        );
+        gateway.callContract(destinationChain, destinationAddress, payload);
+    }
+}
---
+++ src/ContractChainC.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol";
+
+contract ExecutableSampleChainA is AxelarExecutable {
+    string public message;
+    IAxelarGasService public immutable gasService;
+
+    constructor(
+        address gateway_,
+        address gasReceiver_
+    ) AxelarExecutable(gateway_) {
+        gasService = IAxelarGasService(gasReceiver_);
+    }
+
+    // Handles calls created by setAndSend. Updates this contract's value
+    function _execute(
+        string calldata sourceChain_,
+        string calldata sourceAddress_,
+        bytes calldata payload_
+    ) internal override {
+        // Decode the payload to retrieve the new message value
+        (message) = abi.decode(payload_, (string));
+    }
+}

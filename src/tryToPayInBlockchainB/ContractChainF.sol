f8bf7c8390ba4234db089c6fc4310134d1d6e047
cloud1992 <arturocollado@ratherlabs.com>
2024-10-01 16:06:03 -0300

    :sparkles: added new test case...try to pay fees in blockchain B

---
+++ src/tryToPayInBlockchainB/ContractChainF.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol";
+
+contract ExecutableSampleChainF is AxelarExecutable {
+    string public message;
+    IAxelarGasService public immutable gasService;
+
+    constructor(
+        address gateway_,
+        address gasReceiver_
+    ) AxelarExecutable(gateway_) {
+        gasService = IAxelarGasService(gasReceiver_);
+    }
+
+    function estimateFees(
+        string memory destinationChain,
+        string memory destinationAddress,
+        string memory messageToSend,
+        uint gasLimit,
+        string memory destinationChainC,
+        string memory destinationAddressC,
+        string memory messageTochainC
+    ) external view returns (uint) {
+        bytes memory payload = abi.encode(
+            messageToSend,
+            messageTochainC,
+            destinationChainC,
+            destinationAddressC,
+        );
+        return gasService.estimateGasFee(
+            destinationChain,
+            destinationAddress,
+            payload,
+            gasLimit,
+            new bytes(0)
+        );
+    }
+
+    // Call this function to send a message to chain destinationChain and destinationAddress.
+    // Also send send the information to chain destinationChainC and destinationAddressC in order to send a message to chain C from chain B
+    function sendMessage(
+        string memory destinationChain,
+        string memory destinationAddress,
+        string memory messageToSend,
+        uint gasLimit,
+        string memory destinationChainC,
+        string memory destinationAddressC,
+        string memory messageTochainC
+    ) external payable {
+        require(msg.value > 0, "Gas payment is required");
+
+        bytes memory payload = abi.encode(
+            messageToSend,
+            messageTochainC,
+            destinationChainC,
+            destinationAddressC,
+        );
+        _payGasAndExecuteCall(
+            destinationChain,
+            destinationAddress,
+            payload,
+            gasLimit
+        );
+    }
+
+    // I added this internal function to handle stack too deep error
+    function _payGasAndExecuteCall(
+        string memory destinationChain,
+        string memory destinationAddress,
+        bytes memory payload,
+        uint gasLimit
+    ) private {
+        gasService.payGas{value: msg.value}(
+            address(this),
+            destinationChain,
+            destinationAddress,
+            payload,
+            gasLimit,
+            false,
+            msg.sender,
+            new bytes(0)
+        );
+        gateway.callContract(destinationChain, destinationAddress, payload);
+    }
+}
---
+++ src/tryToPayInBlockchainB/ContractChainG.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol";
+
+contract ExecutableSampleChainG is AxelarExecutable {
+    string public message;
+    IAxelarGasService public immutable gasService;
+
+    constructor(
+        address gateway_,
+        address gasReceiver_
+    ) AxelarExecutable(gateway_) {
+        gasService = IAxelarGasService(gasReceiver_);
+    }
+
+    // Handles calls created by setAndSend. Updates this contract's value
+    function _execute(
+        string calldata sourceChain_,
+        string calldata sourceAddress_,
+        bytes calldata payload_
+    ) internal override {
+        // Decode the payload to retrieve the new message value
+        string memory destinationChainC;
+        string memory destinationAddressC;
+        string memory messageTochainC;
+
+        (
+            message,
+            messageTochainC,
+            destinationChainC,
+            destinationAddressC
+        ) = abi.decode(
+            payload_,
+            (string, string, string, string)
+        );
+
+        bytes memory payload = abi.encode(
+            messageTochainC,
+        );
+
+        gasService.payGas{value: address(this).value}(
+            address(this),
+            destinationChainC,
+            destinationAddressC,
+            payload,
+            300_000,
+            false,
+            address(this),
+            new bytes(0)
+        );
+
+        gateway.callContract(destinationChainC, destinationAddressC, payload);
+    }
+}
